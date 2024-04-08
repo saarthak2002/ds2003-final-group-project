d67b86b26a5c434b33b3ef4c1c514ddc84767627
gsilver321 <jgb9fz@virginia.edu>
2024-04-08 12:49:44 -0400

    Start ui for project

--- Group_10_project/app.R
+++ Group_10_project/app.R
@@ -1,49 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+library(bslib)
+library(ggplot2)
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+ui <- page_navbar(
+  title="Virginia Crash Data",
+  underline=T,
+  tabPanel("Plot 1",
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+      sidebarPanel(
+        dateRangeInput("slider1", "Date range", "2015-01-01", "2023-01-01", format="yyyy"),
+        selectInput("countyInput", "Select County", c("County1", "County2")),
+        checkboxGroupInput("pAffect", "Select Injured/Killed", c("Injured", "Killed"))
+      ),
+      mainPanel(plotOutput("plot1"), fluidRow())
     )
+  ),
+  tabPanel("Plot 2",
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("slider1", "Date range", 2015, 2023, 1, value=2015)
+      ),
+      mainPanel(plotOutput("plot2"), fluidRow())
+    )
+  ),
+  tabPanel("Plot 3",
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("slider1", "Date range", 2015, 2023, 1, value=2015)
+      ),
+      mainPanel(plotOutput("plot3"), fluidRow())
+    )
+  ),
+  footer=tags$footer(
+    style = "background-color: #2C3E50; padding: 0px; text-align: center; 
+      position: fixed; bottom: 0; width: 100%; color: white", 
+    "DS 2003 \u00A0 | \u00A0 Group 10 \u00A0 | \u00A0 Gabe Silverstein, Saarthak Gupta, Hasita Nalluri"),
+  theme=bs_theme(
+    bootswatch="flatly",
+    primary="#18BC9C",
+    success="#2C3E50")
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw plots
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$plot1 <- renderPlot({
+      ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
+    })
+    
+    output$plot2 <- renderPlot({
+      ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
+    })
+    
+    output$plot3 <- renderPlot({
+      ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
     })
 }
 
